a8881e20c512650fd0d3d97f2ff3e03671d7fd1e
Marcelo Jose <josedasilvamarcelo@yahoo.com.br>
2019-04-09 21:33:54 -0300

    Acrescentada uma mensagem de vom dia

--- TesteGit/Program.cs
+++ TesteGit/Program.cs
@@ -7,6 +7,9 @@ namespace TesteGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!");
+
+            Console.WriteLine();
         }
     }
 }
